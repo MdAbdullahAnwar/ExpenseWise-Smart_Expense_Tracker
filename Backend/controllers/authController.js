6d3561b0dcc084d77b8e2bb24dd242255b889f76
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-10-06 13:55:42 +0530

    Make the backend api of the login

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -31,3 +31,32 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({ message: "Email and password are required" });
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    return res.status(200).json({
+      message: "User login successful",
+      userId: user.id,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
